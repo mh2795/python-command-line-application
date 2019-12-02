9dbdfc3f989a0766c5ab13fa1a774221522d58e4
Mustafa Hassan <mustafawhassan@gmail.com>
2019-12-02 01:44:37 -0500

    added update method in CRUD

--- main.py
+++ main.py
@@ -33,3 +33,24 @@ def createContacts():
         new_contact = Contact(first_name=name, last_name=surname, phone=number)
         new_contact.save()
     print(f'created {created_num} contacts')
+
+
+def readContacts():
+    listed_num = 0
+    for contact in Contact.select().order_by(Contact.first_name):
+        print(f"{contact.first_name} {contact.last_name} - {contact.phone}")
+        listed_num += 1
+    print(f'listing {listed_num} records')
+    res = input("Would you like to look for a first name? (y or n) ")
+    if res == 'y':
+        name = input("Enter first name: ")
+        record = Contact.select().where(Contact.first_name == name).get()
+        if (record):
+            print(record.first_name)
+            print(record.last_name)
+            print(record.phone)
+        else:
+            print("Sorry there are no records matching that name.")
+    else:
+        pass
+
