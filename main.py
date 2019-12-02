4985f3cabb4295928eaccfb373a824cf94ae1720
Mustafa Hassan <mustafawhassan@gmail.com>
2019-12-02 01:43:45 -0500

    add create method in CRUD

--- main.py
+++ main.py
@@ -17,3 +17,19 @@ class Contact(Model):
 
 db.create_tables([Contact])
 
+
+def createContacts():
+    print("Create New Contact")
+    print("-----Hit Enter without First Name input to Break------")
+    created_num = 0
+    while True:
+        name = input("First Name: ")
+        if name == '':
+            break
+        surname = input("Last Name: ")
+        number = input("Phone: ")
+        print("Contact created!")
+        created_num += 1
+        new_contact = Contact(first_name=name, last_name=surname, phone=number)
+        new_contact.save()
+    print(f'created {created_num} contacts')
